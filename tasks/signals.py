7f52dc60cf38d15e4976531dc3d2ae7375a0b293
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-07-27 16:49:00 +0300

    fix small errors

--- tasks/signals.py
+++ tasks/signals.py
@@ -3,38 +3,19 @@ from django.dispatch import receiver
 from tasks.models import TodoItem, Category, Priority
 from collections import Counter
 from django.db.models import Count
+from django.db.models import F
 
 @receiver(m2m_changed, sender=TodoItem.category.through)
 def task_cats_added(sender, instance, action, model, **kwargs):
-    if action != "post_add":
-        return
-
-    for cat in instance.category.all():
-        slug = cat.slug
-
-        new_count = 0
-        for task in TodoItem.objects.all():
-            new_count += task.category.filter(slug=slug).count()
-
-        Category.objects.filter(slug=slug).update(todos_count=new_count)
-
-
-@receiver(m2m_changed, sender=TodoItem.category.through)
-def task_cats_removed(sender, instance, action, model, **kwargs):
-    if action == "post_remove":
-        cat_counter = Counter()
-        for t in TodoItem.objects.all():
-            for cat in t.category.all():
-                cat_counter[cat.slug] += 1
-
-        for slug, new_count in cat_counter.items():
-            Category.objects.filter(slug=slug).update(todos_count=new_count)
-
-
-    if action == "pre_remove":
+    if action in ["pre_add", "pre_remove"]:
         for cat in instance.category.all():
             slug = cat.slug
-            Category.objects.filter(slug=slug).update(todos_count=0)
+            Category.objects.filter(slug=slug).update(todos_count=F('todos_count') - 1)
+
+    if action in ["post_add", "post_remove"] :
+        for cat in instance.category.all():
+            slug = cat.slug
+            Category.objects.filter(slug=slug).update(todos_count=F('todos_count') + 1)
 
 
 @receiver(post_save, sender=TodoItem)
